2cfd4b8c749809304bbf05f4ebfe782780a1a350
sunaiwen <sunivengg@gmail.com>
2014-11-13 09:35:23 +0800

    fix test bug

--- tests/test.js
+++ tests/test.js
@@ -2,31 +2,29 @@ var expect = chai.expect;
 describe('Scroll2Top', function(){
     var randomYGlobal;
     var randomYBlock;
-    var testBlock;
-    var innerBlock;
+    var testBlock = document.createElement('div');
+    var innerBlock = document.createElement('div');
+
+    innerBlock.style.height = '5000px';
+    testBlock.style.height = '2000px';
+    testBlock.style.overflow = 'auto';
+    testBlock.appendChild(innerBlock);
     beforeEach(function(){
-        testBlock = document.createElement('div');
-        innerBlock = document.createElement('div');
-        innerBlock.style.height = '5000px';
-        testBlock.style.height = '2000px';
-        testBlock.style.overflow = 'auto';
-        testBlock.appendChild(innerBlock);
         document.body.appendChild(testBlock);
-
-        randomYGlobal = Math.round(Math.random() * 5000);
-        window.scrollTo(0, randomYGlobal);
-
-        randomYBlock = Math.round(Math.random() * 5000);
-        testBlock.scrollTop = randomYBlock;
     });
 
     describe('#scrollTo Func', function(){
         it('return the Y coordinate correctly', function(done){
+            randomYGlobal = Math.round(Math.random() * 5000);
+            window.scrollTo(0, randomYGlobal);
+
+            randomYBlock = Math.round(Math.random() * 5000);
+            testBlock.scrollTop = randomYBlock;
             setTimeout(function(){
                 expect(window.testScope.scrollTop(window)).to.equal(randomYGlobal);
                 expect(window.testScope.scrollTop(testBlock)).to.equal(randomYBlock);
                 done();
-            }, 1000);
+            }, 120);
         });
     });
 
@@ -35,11 +33,14 @@ describe('Scroll2Top', function(){
             var re = window.testScope.scroll2Top();
             expect(re).to.equal('error');
         });
-        it('should only execute once when Y coordinate is 0.', function(){
+        it('should only execute once when Y coordinate is 0.', function(done){
             var spy = sinon.spy();
             testBlock.scrollTop = 0;
-            window.testScope.scroll2Top(testBlock, 1200, spy);
-            expect(spy.callCount).to.equal(1);
+            setTimeout(function(){
+                window.testScope.scroll2Top(testBlock, 1200, spy);
+                expect(spy.callCount).to.equal(1);
+                done();
+            }, 200);
         });
         it('should execute more than one time when Y coordinate is not 0.', function(done){
             var spy = sinon.spy();
@@ -50,5 +51,9 @@ describe('Scroll2Top', function(){
                 done();
             }, 1800);
         });
+    });
+
+    afterEach(function(){
+        document.body.removeChild(testBlock);
     });
 });
